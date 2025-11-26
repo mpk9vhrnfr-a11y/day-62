0a78d03039a9649cd0c4d3d80d71474b1765ca3d
sanaa <Sanaakumar07@gmail.com>
2025-11-26 17:25:12 +0530

    c

--- day 62/main.c
+++ day 62/main.c
@@ -1,15 +1,29 @@
-//
-//  main.c
-//  day 62
-//
-//  Created by Sanaa Kumar on 26/11/25.
-//
-
-#include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return EXIT_SUCCESS;
+int main(void) {
+    int n, i, currentSum, maxSum;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements: ");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    currentSum = maxSum = arr[0];
+
+    for(i = 1; i < n; i++) {
+        if(currentSum + arr[i] > arr[i])
+            currentSum += arr[i];
+        else
+            currentSum = arr[i];
+        if(currentSum > maxSum)
+            maxSum = currentSum;
+    }
+
+    printf("%d\n", maxSum);
+    return 0;
 }
+
